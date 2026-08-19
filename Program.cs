ff4297cbc11b8ab000760349c07ce941123f61a2
AhmedEl3araby1 <ahmedhanyah2030@gmail.com>
2026-08-19 11:55:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OOP04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1  Abstraction
+
+            // a) Hiding complex internal details and showing only the essentials the user needs.
+
+            // b) It simplifies code, separates usage from implementation, and gives flexibility to change internals without affecting the rest of the program.
+
+            #endregion
+
+            #region Q2  Abstract Classes vs. Interfaces
+            /*
+            a) Difference:
+            Abstract Class: has both implemented and abstract methods, can have state
+            Interface: defines only required methods, usually no state
+            
+            b) When a class needs to follow more than one behavior, or there's no shared code to reuse.
+             
+             c) Multiple inheritance:
+              Abstract Class: no, only one
+             Interface: yes, multiple
+             
+             */
+            #endregion
+
+
+
+
+        }
+    }
+}
